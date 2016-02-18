da6c8e440e5164ec12439fb978cdc017d4ae0d94
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-02-18 22:10:57 +0200

    Add case if i is multiple of 3

--- FizzBuzzWoof/FizzBuzzWoof.cpp
+++ FizzBuzzWoof/FizzBuzzWoof.cpp
@@ -7,7 +7,12 @@ int main()
 	int i;
 	for (i = 1; i <= 20; i++)
 	{
-		cout << i<< endl;
+		// Check Divisibility by 3
+		if (i % 3 == 0)
+		{
+			cout << "Fizz ";
+		}
 
+		cout << endl;
 	}// for i
 }// main method
